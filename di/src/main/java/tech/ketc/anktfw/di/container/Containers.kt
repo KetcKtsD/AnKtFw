0d9c744149651dab985283b08bcb675367998343
KetcKtsD <serori328@gmail.com>
2018-06-05 15:23:46 +0900

    Container<T>にvalueプロパティを追加

--- di/src/main/java/tech/ketc/anktfw/di/container/Containers.kt
+++ di/src/main/java/tech/ketc/anktfw/di/container/Containers.kt
@@ -3,17 +3,18 @@ package tech.ketc.anktfw.di.container
 import kotlin.reflect.KProperty
 
 interface Container<T : Any> {
+    val value: T
     operator fun getValue(thisRef: Any?, property: KProperty<*>): T
 }
 
 internal class LazyContainer<T : Any>(initializer: () -> T) : Container<T> {
-    private val value by lazy(initializer)
+    override val value by lazy(initializer)
 
     override fun getValue(thisRef: Any?, property: KProperty<*>): T = value
 }
 
 internal class SimpleContainer<T : Any>(initializer: () -> T) : Container<T> {
-    private val value = initializer()
+    override val value = initializer()
 
     override fun getValue(thisRef: Any?, property: KProperty<*>): T = value
 }
