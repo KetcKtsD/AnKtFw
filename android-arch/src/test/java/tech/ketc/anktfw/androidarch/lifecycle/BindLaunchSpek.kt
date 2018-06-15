dc63b0607a1bca9e81a6c66b858ab5d84b86383b
KetcKtsD <serori328@gmail.com>
2018-06-15 10:35:52 +0900

    テストコードが間違っていた

--- android-arch/src/test/java/tech/ketc/anktfw/androidarch/lifecycle/BindLaunchSpek.kt
+++ android-arch/src/test/java/tech/ketc/anktfw/androidarch/lifecycle/BindLaunchSpek.kt
@@ -9,40 +9,46 @@ import org.jetbrains.spek.api.Spek
 import org.jetbrains.spek.api.dsl.it
 import tech.ketc.anktfw.androidarch.croutine.asyncResponse
 import tech.ketc.anktfw.androidarch.croutine.defaultContext
+import tech.ketc.anktfw.androidarch.croutine.successIf
 import kotlin.test.assertFalse
 import kotlin.test.assertTrue
 
 class BindLaunchSpek : Spek({
-    it("expect that the job is bound to LifecycleOwner") {
-        runBlocking {
-            val owner = object : LifecycleOwner {
-                private val mRegistry = LifecycleRegistry(this)
-                override fun getLifecycle(): LifecycleRegistry = mRegistry
-            }
+    fun owner() = object : LifecycleOwner {
+        private val mRegistry = LifecycleRegistry(this)
+        override fun getLifecycle(): LifecycleRegistry = mRegistry
+    }
 
+    it("Expected to be executed without problems") {
+        runBlocking {
+            val owner = owner()
             val registry = owner.lifecycle
             registry.handleLifecycleEvent(Lifecycle.Event.ON_CREATE)
-
-            var executed = false
-            owner.bindLaunch(CommonPool) {
-                asyncResponse(defaultContext) {
-                    100
-                }.await()
-                executed = true
-            }.join()
-            assertTrue(executed)
-
-            executed = false
-            (0..100).forEach {
-                owner.bindLaunch(CommonPool) {
-                    println("execute 2")
-                    asyncResponse(defaultContext) {
-                        println("async start")
-                        registry.handleLifecycleEvent(Lifecycle.Event.ON_DESTROY)
-                        println("owner destroyed")
+            (1..100).forEach {
+                var executed = false
+                owner.bindLaunch(coroutineContext) {
+                    val response = asyncResponse(defaultContext) {
                         100
                     }.await()
-                    println("destroyed")
+                    response.successIf { executed = true }
+                }.join()
+                assertTrue(executed)
+            }
+        }
+    }
+
+    it("expect that the job is bound to LifecycleOwner") {
+        runBlocking {
+            (1..100).forEach {
+                var executed = false
+                val owner = owner()
+                val registry = owner.lifecycle
+                registry.handleLifecycleEvent(Lifecycle.Event.ON_CREATE)
+                owner.bindLaunch(coroutineContext) {
+                    asyncResponse(defaultContext) {
+                        registry.handleLifecycleEvent(Lifecycle.Event.ON_DESTROY)
+                        100
+                    }.await()
                     executed = true
                 }.join()
                 assertFalse(executed)
