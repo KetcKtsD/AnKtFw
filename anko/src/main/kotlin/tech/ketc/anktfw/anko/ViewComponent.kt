015f5d0cdecc801a0659c321fce59ca9d66ed417
KetcKtsD <serori328@gmail.com>
2018-06-18 13:14:57 +0900

    ViewManager.componentを使用した際、自動的にrootのViewにIDを割り振るように変更

--- anko/src/main/kotlin/tech/ketc/anktfw/anko/ViewComponent.kt
+++ anko/src/main/kotlin/tech/ketc/anktfw/anko/ViewComponent.kt
@@ -29,10 +29,17 @@ private val DEFAULT_INIT: View.() -> Unit = {}
  * @return created View
  */
 fun <R : View> ViewManager.component(component: ViewComponent<R>,
-                                     init: R.() -> Unit = DEFAULT_INIT): R = when (this) {
-    is ViewGroup -> component.createView(context).apply(init).also(::addView)
-    is AnkoContext<*> -> component.createView(ctx).apply(init).also { addView(it, null) }
-    else -> throw AnkoException("$this is the wrong parent")
+                                     init: R.() -> Unit = DEFAULT_INIT): R {
+    val initialize: R.() -> Unit = {
+        init()
+        if (id == View.NO_ID) id = View.generateViewId()
+    }
+
+    return when (this) {
+        is ViewGroup -> component.createView(context).apply(initialize).also(::addView)
+        is AnkoContext<*> -> component.createView(ctx).apply(initialize).also { addView(it, null) }
+        else -> throw AnkoException("$this is the wrong parent")
+    }
 }
 
 /**
