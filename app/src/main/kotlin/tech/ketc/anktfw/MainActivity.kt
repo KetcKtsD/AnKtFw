3bd3f970bc7f44046cd4979f51c733ab9dd5c080
KetcKtsD <serori328@gmail.com>
2018-06-28 17:31:47 +0900

    こうのほうが良くね？

--- app/src/main/kotlin/tech/ketc/anktfw/MainActivity.kt
+++ app/src/main/kotlin/tech/ketc/anktfw/MainActivity.kt
@@ -1,14 +1,32 @@
 package tech.ketc.anktfw
 
 import android.os.Bundle
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import org.jetbrains.anko.setContentView
+import org.jetbrains.anko.startActivity
+import tech.ketc.anktfw.androidarch.AsyncSampleActivity
+import tech.ketc.anktfw.animation.AnimationSampleActivity
 
 class MainActivity : AppCompatActivity(), IMainUI by MainUI() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(this)
+        initializeUI()
+    }
+
+    private fun initializeUI() {
         setSupportActionBar(appbarComponent.toolbar)
+        onNavigationItemSelected(::onNavigationItemSelected)
+    }
+
+    private fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.start_animation_sample -> startActivity<AnimationSampleActivity>()
+            R.id.start_arch_sample -> startActivity<AsyncSampleActivity>()
+            R.id.start_di_sample -> startActivity<AsyncSampleActivity>()
+        }
+        return false
     }
 }
--- app/src/main/kotlin/tech/ketc/anktfw/MainUI.kt
+++ app/src/main/kotlin/tech/ketc/anktfw/MainUI.kt
@@ -1,19 +1,15 @@
 package tech.ketc.anktfw
 
 import android.annotation.SuppressLint
-import android.content.Context
 import android.view.Gravity
 import android.view.MenuItem
 import android.view.View
 import android.widget.TextView
 import androidx.drawerlayout.widget.DrawerLayout
 import org.jetbrains.anko.*
-import tech.ketc.anktfw.androidarch.AsyncSampleActivity
-import tech.ketc.anktfw.animation.AnimationSampleActivity
 import tech.ketc.anktfw.anko.UI
 import tech.ketc.anktfw.anko.bindView
 import tech.ketc.anktfw.anko.component
-import tech.ketc.anktfw.di.DISampleActivity
 import tech.ketc.anktfw.util.drawerLayout
 import tech.ketc.anktfw.util.menuId
 import tech.ketc.anktfw.util.navigationView
@@ -22,26 +18,29 @@ import tech.ketc.anktfw.util.navigationView
 interface IMainUI : UI<MainActivity, DrawerLayout> {
     val textView: TextView
     val appbarComponent: AppbarComponent
+
+    fun onNavigationItemSelected(func: (MenuItem) -> Boolean)
 }
 
 class MainUI : IMainUI {
+
     override lateinit var root: DrawerLayout
         private set
 
-    private val textViewId = View.generateViewId()
-    override val textView: TextView by bindView(textViewId)
+    private val mTextViewId = View.generateViewId()
+    override val textView: TextView by bindView(mTextViewId)
 
     override val appbarComponent: AppbarComponent = SimpleAppbarComponent()
 
     @SuppressLint("SetTextI18n")
-    private val mainContent = component {
+    private val mMainContent = component {
         relativeLayout {
             component(appbarComponent) {
                 id = View.generateViewId()
             }.lparams(matchParent, wrapContent)
 
             textView {
-                id = textViewId
+                id = mTextViewId
                 text = "Hello World!!"
             }.lparams(wrapContent, wrapContent) {
                 centerInParent()
@@ -49,32 +48,25 @@ class MainUI : IMainUI {
         }
     }
 
-    private val navigationContent = component {
+    private val mNavigationContent = component {
         navigationView {
             menuId = R.menu.navigation_main
-            setNavigationItemSelectedListener { onNavigationItemSelected(it) }
         }
     }
 
-    @SuppressLint("SetTextI18n")
     override fun createView(ui: AnkoContext<MainActivity>) = with(ui) {
         drawerLayout {
             root = this
 
-            component(mainContent).lparams(matchParent, matchParent)
+            component(mMainContent).lparams(matchParent, matchParent)
 
-            component(navigationContent).lparams(matchParent, matchParent) {
+            component(mNavigationContent).lparams(matchParent, matchParent) {
                 gravity = Gravity.START
             }
         }
     }
 
-    private fun Context.onNavigationItemSelected(item: MenuItem): Boolean {
-        when (item.itemId) {
-            R.id.di_sample -> startActivity<DISampleActivity>()
-            R.id.arch_sample -> startActivity<AsyncSampleActivity>()
-            R.id.animation_sample -> startActivity<AnimationSampleActivity>()
-        }
-        return false
+    override fun onNavigationItemSelected(func: (MenuItem) -> Boolean) {
+        mNavigationContent.root.setNavigationItemSelectedListener(func)
     }
 }
