39a8797e2ed935acd9fdf0843e3bb4d710c84558
ketcktsd <serori328@gmail.com>
2018-08-25 23:51:01 +0900

    kotlinx.coroutinesのupdate

--- android-arch/src/androidTest/kotlin/io/github/ketcktsd/anktfw/androidarch/lifecycle/BindLaunchSpek.kt
+++ android-arch/src/androidTest/kotlin/io/github/ketcktsd/anktfw/androidarch/lifecycle/BindLaunchSpek.kt
@@ -1,21 +1,19 @@
 package io.github.ketcktsd.anktfw.androidarch.lifecycle
 
-//todo Inappropriate checking exception is thrown inside runBlocking
-
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.LifecycleRegistry
-//import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.runBlocking
 import org.jetbrains.spek.api.Spek
-//import org.jetbrains.spek.api.dsl.it
+import org.jetbrains.spek.api.dsl.it
 import org.junit.platform.runner.JUnitPlatform
 import org.junit.runner.RunWith
-//import io.github.ketcktsd.anktfw.androidarch.croutine.asyncResponse
-//import io.github.ketcktsd.anktfw.androidarch.croutine.defaultContext
-//import io.github.ketcktsd.anktfw.androidarch.croutine.successIf
-//import kotlin.coroutines.coroutineContext
-//import kotlin.test.assertFalse
-//import kotlin.test.assertTrue
+import io.github.ketcktsd.anktfw.androidarch.croutine.asyncResponse
+import io.github.ketcktsd.anktfw.androidarch.croutine.defaultContext
+import io.github.ketcktsd.anktfw.androidarch.croutine.successIf
+import kotlin.coroutines.coroutineContext
+import kotlin.test.assertFalse
+import kotlin.test.assertTrue
 
 @RunWith(JUnitPlatform::class)
 class BindLaunchSpek : Spek({
@@ -24,40 +22,40 @@ class BindLaunchSpek : Spek({
         override fun getLifecycle(): LifecycleRegistry = mRegistry
     }
 
-//    it("Expected to be executed without problems") {
-//        runBlocking {
-//            val owner = owner()
-//            val registry = owner.lifecycle
-//            registry.handleLifecycleEvent(Lifecycle.Event.ON_CREATE)
-//            (1..100).forEach {
-//                var executed = false
-//                owner.bindLaunch(coroutineContext) {
-//                    val response = asyncResponse(defaultContext()) {
-//                        100
-//                    }.await()
-//                    response.successIf { executed = true }
-//                }.join()
-//                assertTrue(executed)
-//            }
-//        }
-//    }
-//
-//    it("expect that the job is bound to LifecycleOwner") {
-//        runBlocking {
-//            (1..100).forEach {
-//                var executed = false
-//                val owner = owner()
-//                val registry = owner.lifecycle
-//                registry.handleLifecycleEvent(Lifecycle.Event.ON_CREATE)
-//                owner.bindLaunch(coroutineContext) {
-//                    asyncResponse(defaultContext()) {
-//                        registry.handleLifecycleEvent(Lifecycle.Event.ON_DESTROY)
-//                        100
-//                    }.await()
-//                    executed = true
-//                }.join()
-//                assertFalse(executed)
-//            }
-//        }
-//    }
+    it("expected to be executed without problems") {
+        runBlocking {
+            val owner = owner()
+            val registry = owner.lifecycle
+            registry.handleLifecycleEvent(Lifecycle.Event.ON_CREATE)
+            (1..100).forEach { _ ->
+                var executed = false
+                owner.bindLaunch(coroutineContext) {
+                    val response = asyncResponse(defaultContext()) {
+                        100
+                    }.await()
+                    response.successIf { executed = true }
+                }.join()
+                assertTrue(executed)
+            }
+        }
+    }
+
+    it("expect that the job is bound to LifecycleOwner") {
+        runBlocking {
+            (1..100).forEach {
+                var executed = false
+                val owner = owner()
+                val registry = owner.lifecycle
+                registry.handleLifecycleEvent(Lifecycle.Event.ON_CREATE)
+                owner.bindLaunch(coroutineContext) {
+                    asyncResponse(defaultContext()) {
+                        registry.handleLifecycleEvent(Lifecycle.Event.ON_DESTROY)
+                        100
+                    }.await()
+                    executed = true
+                }.join()
+                assertFalse(executed)
+            }
+        }
+    }
 })
