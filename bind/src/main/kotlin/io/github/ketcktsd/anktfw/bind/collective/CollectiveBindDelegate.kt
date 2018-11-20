7da596e19fb4b31ab8c591d92dc7a61f4261f7df
KetcKtsD <serori328@gmail.com>
2018-11-20 17:27:03 +0900

    よーっし全部インターフェースに切り出せたぞ

--- bind/src/main/kotlin/io/github/ketcktsd/anktfw/bind/collective/CollectiveBindDelegate.kt
+++ bind/src/main/kotlin/io/github/ketcktsd/anktfw/bind/collective/CollectiveBindDelegate.kt
@@ -1,40 +1,43 @@
 package io.github.ketcktsd.anktfw.bind.collective
 
-import io.github.ketcktsd.anktfw.bind.property.BindableProperty
+import io.github.ketcktsd.anktfw.bind.property.ObservableProperty
 
-class CollectiveBindDelegate<T>(private var value: T,
-                                private val bindables: Array<BindableProperty<T>>) {
+interface CollectiveBindDelegate<T> {
+    fun set(value: T)
+    fun get(): T
+}
+
+internal class CollectiveBindDelegateImpl<T>(private var value: T,
+                                    private val observables: Array<ObservableProperty<T>>) : CollectiveBindDelegate<T> {
 
     private var mInitialized = false
 
     init {
-        bindables.forEach {
-            it.onChange = ::onChange
+        observables.forEach {
+            it.addListener(::onChange)
         }
     }
 
-    fun set(value: T) {
+    override fun set(value: T) {
         initialize()
         this.value = value
-        bindables.forEach { it.value = value }
+        observables.forEach { it.value = value }
     }
 
-    fun get(): T {
+    override fun get(): T {
         initialize()
         return value
     }
 
-    private fun onChange(property: BindableProperty<T>) {
-        this.value = property.value
-        bindables.asSequence()
-                .filterNot { it == property }
-                .forEach { it.set(value) }
+    private fun onChange(value: T) {
+        this.value = value
+        observables.forEach { it.value = value }
         if (!mInitialized) mInitialized = true
     }
 
     private fun initialize() {
         if (mInitialized) return
         mInitialized = true
-        bindables.forEach { it.value = value }
+        observables.forEach { it.value = value }
     }
 }
---
+++ bind/src/main/kotlin/io/github/ketcktsd/anktfw/bind/property/ObservableProperty.kt
@@ -0,0 +1,33 @@
+package io.github.ketcktsd.anktfw.bind.property
+
+interface ObservableProperty<T> {
+    var value: T
+    fun addListener(listener: (T) -> Unit)
+    fun removeListener(listener: (T) -> Unit)
+    fun clearListener(listener: (T) -> Unit)
+}
+
+private class ObservablePropertyImpl<T>(internal val set: (value: T) -> Unit,
+                                internal val get: () -> T) : ObservableProperty<T> {
+    override var value: T
+        set(value) {
+            if (get() == value) return
+            set(value)
+            mListeners.forEach { it(value) }
+        }
+        get() = get()
+
+    private val mListeners: MutableList<(T) -> Unit> = ArrayList()
+
+    override fun addListener(listener: (T) -> Unit) {
+        mListeners.add(listener)
+    }
+
+    override fun removeListener(listener: (T) -> Unit) {
+        mListeners.remove(listener)
+    }
+
+    override fun clearListener(listener: (T) -> Unit) = mListeners.clear()
+}
+
+fun <T> bindable(set: (value: T) -> Unit, get: () -> T): ObservableProperty<T> = ObservablePropertyImpl(set, get)
