fb0834f83c219dc08878b661fbcdabbeaad39b6d
ketcktsd <serori328@gmail.com>
2019-07-27 03:17:11 +0900

    set compatibility

--- build.gradle.kts
+++ build.gradle.kts
@@ -28,6 +28,8 @@ subprojects {
 
     tasks {
         withType<KotlinCompile> {
+            sourceCompatibility = "1.8"
+            targetCompatibility = "1.8"
             kotlinOptions {
                 jvmTarget = "1.8"
                 freeCompilerArgs = listOf(
