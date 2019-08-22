edde699d8a34565cb2fb2dd80440945a6a03e8fb
ketcktsd <serori328@gmail.com>
2019-08-22 21:19:37 +0900

    update version to 1.1.1

--- android_sdk_settings.gradle.kts
+++ android_sdk_settings.gradle.kts
@@ -5,6 +5,6 @@ subprojects {
     val targetAndroidSdkVersion by extra { 29 }
     val androidBuildToolVersion by extra { "29.0.2" }
 
-    val libraryVersionCode by extra { 1 }
-    val libraryVersionName by extra { "1.1.0" }
+    val libraryVersionCode by extra { 2 }
+    val libraryVersionName by extra { "1.1.1" }
 }
